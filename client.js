4c8fed10b09495ac8dbe054beda6f84869487221
Sam Musso <sam@alpinelaboratories.com>
2016-11-17 12:05:08 -0700

    Start of streaming code

--- client.js
+++ client.js
@@ -1,6 +1,14 @@
 var socket = require('socket.io-client')('http://10.1.10.124');
+var ss = require('socket.io-stream');
+var fs = require('fs');
+var filename = 'photo.jpg';
+var stream = ss.createStream();
+
 socket.on('connect', function(){
   console.log('We are in business!');
+  ss(socket).emit('photo-added', stream, {name: filename});
+  fs.createReadStream(filename).pipe(stream);
 });
+
 socket.on('event', function(data){});
 socket.on('disconnect', function(){});
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
-
+var ss = require('socket.io-stream');
+var path = require('path');
+var fs = require('fs');
 var PORT = 80;
 
 io.on('connection', function(socket) {
   console.log("Connection succesful!");
+  ss(socket).on('photo-added', function(stream, data){
+    var filename = path.basename(data.name);
+    stream.pipe(fs.createWriteStream(filename));
+  });
   socket.on('disconnect', function(){
     console.log("Client disconnected");
   });
