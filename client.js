f31e041a0e73a8fe0a0a3c9f7ae90b742cb832a0
Sam Musso <sam@alpinelaboratories.com>
2016-11-17 10:29:25 -0700

    Still will not connect

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ var io = require('socket.io-client');
 
 console.log("Attempting to connect to 1.10.1.124/photo");
 
-var socket = io.connect('1.10.1.124/photo', {port: 48626});
+var socket = io.connect('1.10.1.124', {port: 48626});
 
 socket.on('connect', function() {
     console.log("We're in business!");
--- test-server.js
+++
@@ -1,5 +0,0 @@
-var io = require('socket.io')();
-io.on('connection', function(client){
-  console.log("We're in business!");
-});
-io.listen(42058);
