d7bdc3591b3d3d27fce83a81bccf3a6be92b9018
Sam Musso <sam.musso90@gmail.com>
2016-11-17 18:27:34 +0000

    Client not finding server

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
 var io = require('socket.io-client');
 
-console.log("Attempting to connect to 1.10.1.124/photo");
-
-var socket = io.connect('1.10.1.124', {port: 48626});
+var socket = io.connect('http://10.1.10.124', {port: 48626});
 
 socket.on('connect', function() {
     console.log("We're in business!");
